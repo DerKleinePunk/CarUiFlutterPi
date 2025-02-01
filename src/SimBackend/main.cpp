b5097ace853111cfe89b96b7f908134aa24d8581
DerKleinePunk <roboter@mine-robo.de>
2025-02-01 18:43:17 +0100

    gRPC Build and Start

--- src/SimBackend/main.cpp
+++ src/SimBackend/main.cpp
@@ -17,11 +17,11 @@
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
-#include "absl/flags/flag.h"
-#include "absl/flags/parse.h"
-#include "absl/strings/str_format.h"
+#include <absl/flags/flag.h>
+#include <absl/flags/parse.h>
+#include <absl/strings/str_format.h>
 
-#include <generated/helloworld.grpc.pb.h>
+#include <helloworld.grpc.pb.h>
 
 #include "../common/version.hpp"
 #include "../common/easylogging/easylogging++.h"
@@ -30,6 +30,37 @@
 char socketPath[] = "/tmp/SimBackend.sock";
 
 INITIALIZE_EASYLOGGINGPP
+
+// Logic and data behind the server's behavior.
+class GreeterServiceImpl final : public helloworld::Greeter::Service {
+  grpc::Status SayHello(grpc::ServerContext* context, const helloworld::HelloRequest* request,
+                  helloworld::HelloReply* reply) override {
+    std::string prefix("Hello ");
+    reply->set_message(prefix + request->name());
+    return grpc::Status::OK;
+  }
+};
+
+void RunServer(uint16_t port) {
+  std::string server_address = absl::StrFormat("0.0.0.0:%d", port);
+  GreeterServiceImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  grpc::ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
 
 int main(int argc, char** argv)
 {
@@ -67,13 +98,15 @@ int main(int argc, char** argv)
         //Todo Error
     }
 
+    RunServer(5001);
+
     std::cout << "Enter q to Stop" << std::endl;
 
     std::string input;
     std::cin >> input;
-    while(input != "q") {
+    /*while(input != "q") {
         std::cin >> input;
-    }
+    }*/
 
     delete socketController;
     
